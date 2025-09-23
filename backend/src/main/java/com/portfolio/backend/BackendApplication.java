facdc39c7534cb830c3f769670494ad45a1c40b6
PILLYI G SHANKAR <148607888+shankar5455@users.noreply.github.com>
2025-09-23 12:18:19 +0530

    Update BackendApplication.java

--- backend/src/main/java/com/portfolio/backend/BackendApplication.java
+++ backend/src/main/java/com/portfolio/backend/BackendApplication.java
@@ -2,12 +2,19 @@ package com.portfolio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    // Configure method for WAR deployment
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BackendApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
